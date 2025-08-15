8b2504e3be86e5072b20abefc60d6d6f346758b2
Giorgos Kalogeitonas <gkalog2@gmail.com>
2025-08-15 10:55:36 +0300

    Remove WiFi network scanning from loop function

--- src/main.cpp
+++ src/main.cpp
@@ -97,27 +97,6 @@ void loop()
     Serial.println("Current IP address: ");
     Serial.println(WiFi.localIP());
 
-    // WiFi.scanNetworks will return the number of networks found
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-            // Print SSID and RSSI for each network found
-            Serial.print(i + 1);
-            Serial.print(": ");
-            Serial.print(WiFi.SSID(i));
-            Serial.print(" (");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print(")");
-            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-            delay(10);
-        }
-    }
-    Serial.println("");
 
     // Wait a bit before scanning again
     delay(5000);
